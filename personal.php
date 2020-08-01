74d474dc2a4ba83bfc4aab7e30b5664f42e85288
iziyodjon <Ziyodjon85>
2020-08-01 12:42:28 +0500

    Оставил функцию userExit() чтобы красиво смотрелось и было понятно )

--- personal.php
+++ personal.php
@@ -3,15 +3,7 @@ session_start();
 require_once ('libs/func.php');
 
 // Если user == out то выходим из системы
-if($_GET['user'] == 'out')
-{
-    $_SESSION['login'] = '';
-    $_SESSION['auth'] = 0;
-    setcookie("login","",time()-3600);
-    setcookie("key","",time()-3600);
-    header('Location: /');
-}
-
+userExit($_GET['user']);
 ?>
 <h1>Здравствуйте <?= ($_SESSION['login']) ? ucfirst($_SESSION['login']) : ucfirst($_COOKIE['login']);?></h1>
 <a href="/personal.php?user=out"> Выйти из системы</a>
