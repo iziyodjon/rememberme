af87654e34566d0f050e22a3f2e0a55f34a7c3ea
iziyodjon <Ziyodjon85>
2020-08-01 12:09:51 +0500

    Изменил логику

--- personal.php
+++ personal.php
@@ -1,17 +1,17 @@
 <?php
 session_start();
+require_once ('libs/func.php');
 
+// Если user == out то выходим из системы
 if($_GET['user'] == 'out')
 {
     $_SESSION['login'] = '';
-    $_SESSION['auth'] = false;
-}
-
-if (empty($_SESSION['auth']) or $_SESSION['auth'] == false)
-{
+    $_SESSION['auth'] = 0;
+    setcookie("login","",time()-3600);
+    setcookie("key","",time()-3600);
     header('Location: /');
 }
 
 ?>
-
-<a href="/personal.php?user=out">Выйти из системы</a>
+<h1>Здравствуйте <?= ($_SESSION['login']) ? ucfirst($_SESSION['login']) : ucfirst($_COOKIE['login']);?></h1>
+<a href="/personal.php?user=out"> Выйти из системы</a>
